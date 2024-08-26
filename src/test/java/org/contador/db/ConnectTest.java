49cb1fb01f6b5d2d89b0bf62cea6ebc4434480fd
kelve <kelvenunes123@gmail.com>
2024-08-26 11:42:55 -0300

    iniciando processo de auttenticação

---
+++ src/test/java/org/contador/db/ConnectTest.java
@@ -0,0 +1,12 @@
+package org.contador.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectTest {
+
+    @Test
+    void connection() {
+    }
+}
